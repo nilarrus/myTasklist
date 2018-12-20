58fc27247eb2918c03895ec96fff7be91324fc3e
nilarrus <nilarrus69@gmail.com>
2018-12-20 16:40:09 +0100

    turbio

--- index.php
+++ index.php
@@ -9,8 +9,33 @@
     <p>My task list</p>
     <?php echo "
     <div>
-    <p>Nova tasca:</p><input type='text' name="" value="">
+    <p>Nova tasca:</p>
+<form>
+    <input type='text' name='nom'>
+    <input type='submit'>
+</form>
     </div>
     "; ?>
   </body>
+<!--
 </html>
+<head></head><body><h1>Floating beyond... TASKLIST</h1>
+
+<form>
+	Nova tasca:
+	<input name="nom">
+	<input type="submit">
+</form>
+
+<p>Tasques pendents:</p><ul>
+<li>task23 <a href="?fet=70">Fet</a> <a href="?esborra=70">Esborra</a></li>
+<li>quedar per fer unes birres <a href="?fet=63">Fet</a> <a href="?esborra=63">Esborra</a></li>
+<li>hola <a href="?fet=76">Fet</a> <a href="?esborra=76">Esborra</a></li>
+</ul>
+Tasques fetes:<p></p>
+<ul>
+<li>hotla <a href="?desfes=75">Desfes</a> <a href="?esborra=75">Esborra</a></li>
+<li>Prueba <a href="?desfes=73">Desfes</a> <a href="?esborra=73">Esborra</a></li>
+<li>kolo <a href="?desfes=74">Desfes</a> <a href="?esborra=74">Esborra</a></li>
+</ul></body>
+-->
