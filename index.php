76f3ceb571515696c79cd06dccefc102c23d2863
nilarrus <nilarrus69@gmail.com>
2019-01-22 19:52:10 +0100

    conexion99

--- index.php
+++ index.php
@@ -8,29 +8,37 @@
 //  $db = parse_url(getenv("DATABASE_URL"));
 $dbconn = pg_connect("host=ec2-54-243-150-10.compute-1.amazonaws.com dbname=ddufqhqeeesvcb user=nkbozhglxeacba password=44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316")
 or die('No se ha podido conectar: '.pg_last_error());
-/*$PDO = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db[""],
-    $db["5432"],
-    $db["nkbozhglxeacba"],
-    $db["44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316"],
-    $db["ddufqhqeeesvcb"])
-));
-$Query = $PDO -> ("SELECT * FROM mytask;");
-$Query ->execute();
-$resu = $Query ->rowCount();
-*/
+$query = 'SELECT * FROM mytask';
+$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
  ?>
   <body>
     <h1>PLUS ULTRA!! TASKAS EDITYON</h1>
-    <?php echo "
+    <?php
+    // Imprimiendo los resultados en HTML
+      echo "<table>\n";
+      while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+          echo "\t<tr>\n";
+          foreach ($line as $col_value) {
+              echo "\t\t<td>$col_value</td>\n";
+          }
+          echo "\t</tr>\n";
+      }
+      echo "</table>\n";
+
+      // Liberando el conjunto de resultados
+      pg_free_result($result);
+
+      // Cerrando la conexión
+      pg_close($dbconn);
+
+    echo "
     <div>
     <form>
         Nova tasca:
         <input type='text' name='nom'>
         <input type='submit'>
-    </form>
-    </div>
-    $resu
-    "; ?>
+    </form>";
+    echo "</div>";
+    echo $resu;
+   ?>
   </body>
