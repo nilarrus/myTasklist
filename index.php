732794c12b498d43a086328086666f49121cde70
nilarrus <nilarrus69@gmail.com>
2019-01-22 20:22:33 +0100

    conexion...1

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <?php
     echo "
     <div>
-    <form action='index.html' method='post'>
+    <form action='index.php' method='post'>
         Nova tasca:
         <input type='text' name='nom'>
         <input type='submit' value='nova tasca'>
