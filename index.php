0b4cba092436c3e99aae00aecfdb31c26dd17a05
nilarrus <nilarrus69@gmail.com>
2019-01-22 20:13:35 +0100

    conexion109

--- index.php
+++ index.php
@@ -2,17 +2,29 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    <title>My Task List</title>
+    <title>My Task List XD</title>
   </head>
   <?php
-//  $db = parse_url(getenv("DATABASE_URL"));
-$dbconn = pg_connect("host=ec2-54-243-150-10.compute-1.amazonaws.com dbname=ddufqhqeeesvcb user=nkbozhglxeacba password=44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316")
-or die('No se ha podido conectar: '.pg_last_error());
-$result = pg_exec('SELECT * FROM mytask');
- ?>
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    alert($_POST['nom']);
+  }
+
+    //  $db = parse_url(getenv("DATABASE_URL"));
+    $dbconn = pg_connect("host=ec2-54-243-150-10.compute-1.amazonaws.com dbname=ddufqhqeeesvcb user=nkbozhglxeacba password=44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316")
+    or die('No se ha podido conectar: '.pg_last_error());
+    $result = pg_exec('SELECT * FROM mytask');
+  ?>
   <body>
     <h1>PLUS ULTRA!! TASKAS EDITYON</h1>
     <?php
+    echo "
+    <div>
+    <form>
+        Nova tasca:
+        <input type='text' name='nom'>
+        <input type='submit' value='nova tasca'>
+    </form>";
+    echo "</div>";
     // Imprimiendo los resultados en HTML
       echo "<table>\n";
       while ($row = pg_fetch_array($result)) {
@@ -27,14 +39,5 @@ $result = pg_exec('SELECT * FROM mytask');
 
       // Cerrando la conexión
       pg_close($dbconn);
-
-    echo "
-    <div>
-    <form>
-        Nova tasca:
-        <input type='text' name='nom'>
-        <input type='submit'>
-    </form>";
-    echo "</div>";
    ?>
   </body>
