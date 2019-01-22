1c3fee2a27f73ac0e89677f9d689000fd7293bc9
nilarrus <nilarrus69@gmail.com>
2019-01-22 20:07:56 +0100

    conexion100

--- index.php
+++ index.php
@@ -8,19 +8,16 @@
 //  $db = parse_url(getenv("DATABASE_URL"));
 $dbconn = pg_connect("host=ec2-54-243-150-10.compute-1.amazonaws.com dbname=ddufqhqeeesvcb user=nkbozhglxeacba password=44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316")
 or die('No se ha podido conectar: '.pg_last_error());
-$query = 'SELECT * FROM mytask';
-$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+$result = pg_exec('SELECT * FROM mytask');
  ?>
   <body>
     <h1>PLUS ULTRA!! TASKAS EDITYON</h1>
     <?php
     // Imprimiendo los resultados en HTML
       echo "<table>\n";
-      while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+      while ($row = pg_fetch_array($result)) {
           echo "\t<tr>\n";
-          foreach ($line as $col_value) {
-              echo "\t\t<td>$col_value</td>\n";
-          }
+          echo "\t\t<td>".$row['descripcio']"</td>\n";
           echo "\t</tr>\n";
       }
       echo "</table>\n";
@@ -39,6 +36,5 @@ $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
         <input type='submit'>
     </form>";
     echo "</div>";
-    echo $resu;
    ?>
   </body>
