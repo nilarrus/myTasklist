5ed788fb9f9904682e587875e5cd8d2858a15cb8
nilarrus <nilarrus69@gmail.com>
2019-01-22 19:44:18 +0100

    conexion6

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
   </head>
   <?php
 //  $db = parse_url(getenv("DATABASE_URL"));
-$dbconn = pg_connect("host=localhost dbname=ddufqhqeeesvcb user=nkbozhglxeacba password=44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316") or die('No se ha podido conectar: ' . pg_last_error());
+$dbconn = pg_connect("host=ec2-54-243-150-10.compute-1.amazonaws.com dbname=ddufqhqeeesvcb user=nkbozhglxeacba password=44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316")
+or die('No se ha podido conectar: '.pg_last_error());
 /*$PDO = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db[""],
