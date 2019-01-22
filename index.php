7fbe0705fba76a26ffe610b9de7d4c5cd31df2de
nilarrus <nilarrus69@gmail.com>
2019-01-22 19:42:19 +0100

    conexion6

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <title>My Task List</title>
   </head>
   <?php
-  $db = parse_url(getenv("DATABASE_URL"));
-
-$PDO = new PDO("pgsql:" . sprintf(
+//  $db = parse_url(getenv("DATABASE_URL"));
+$dbconn = pg_connect("host=localhost dbname=ddufqhqeeesvcb user=nkbozhglxeacba password=44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316") or die('No se ha podido conectar: ' . pg_last_error());
+/*$PDO = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["ec2-54-243-150-10.compute-1.amazonaws.com"],
+    $db[""],
     $db["5432"],
     $db["nkbozhglxeacba"],
     $db["44f66b71869d41bf6b911906f1d0820ff112e8f1a1f431cd325b07ee234c2316"],
@@ -18,7 +18,7 @@ $PDO = new PDO("pgsql:" . sprintf(
 $Query = $PDO -> ("SELECT * FROM mytask;");
 $Query ->execute();
 $resu = $Query ->rowCount();
-
+*/
  ?>
   <body>
     <h1>PLUS ULTRA!! TASKAS EDITYON</h1>
