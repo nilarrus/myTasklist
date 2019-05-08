666fc72016ee490eb82f6eb26701d7d910a66e4e
Nilarrus <nilarrus69@gmail.com>
2019-05-08 19:00:23 +0200

    gestior general 3

--- index.php
+++ index.php
@@ -19,31 +19,23 @@
 
  //echo "GET: ".var_dump($_GET);
  if(isset( $_GET['delete'] )){
-   echo "En el get";
+   //echo "En el get";
    $dbconn = pg_connect("host=ec2-107-21-224-76.compute-1.amazonaws.com dbname=d9tf9mvi6tvf71 user=xrnnfbpijdpmin password=e2f25edc7569735ac66c311c993f760c258fbdbb19a97e7650d1d6524cf9da80")
    or die('No se ha podido conectar: '.pg_last_error());
    $id = $_GET['delete'];
-   echo $id;
+   
    $result = pg_prepare($dbconn,"my_query",'DELETE FROM mytasks WHERE id LIKE $1;');
-   echo $result;
+   
    $result = pg_execute($dbconn,"my_query",$id);
  }
   if($_SERVER["REQUEST_METHOD"] == "POST"){
-      echo "En el post";
-    $dbconn = pg_connect("host=ec2-107-21-224-76.compute-1.amazonaws.com dbname=d9tf9mvi6tvf71 user=xrnnfbpijdpmin password=e2f25edc7569735ac66c311c993f760c258fbdbb19a97e7650d1d6524cf9da80")
-    or die('No se ha podido conectar: '.pg_last_error());
     $nom = $_POST['nom'];
-    $result = pg_prepare($dbconn,"my_query",'INSERT INTO mytasks(descripcio,hecho) VALUES ($1,$2);');
-    $result = pg_execute($dbconn,"my_query",array($nom,0));
+    $query = $db->prepare("INSERT INTO TaskList (task,state) VALUES ('".$nom."',0)");
+    $query->execute();
   }
-
-    
+   
     $result = $db->query("SELECT * FROM mytasks");
-    /*
-    $dbconn = pg_connect("host=ec2-107-21-224-76.compute-1.amazonaws.com dbname=d9tf9mvi6tvf71 user=xrnnfbpijdpmin password=e2f25edc7569735ac66c311c993f760c258fbdbb19a97e7650d1d6524cf9da80")
-    or die('No se ha podido conectar: '.pg_last_error());
-    $result = pg_exec($dbconn,'SELECT * FROM mytasks');*/
-    echo "Resultat: ".var_dump($result);
+    //echo "Resultat: ".var_dump($result);
   ?>
   <body>
     <h1>TASKAS EDITYON</h1>
@@ -70,23 +62,6 @@
           }         
           echo "\t</tr>\n";
       }
-      /*
-      while ($row = pg_fetch_array($result)) {
-          echo "\t<tr>\n";          
-          echo "\t\t<td>--".$row['id']."--</td>\n";
-          echo "\t\t<td>".$row['descripcio']." <a href='index.php?delete=".$row['id']."'>Eliminar</a></td>\n";
-          if($row['hecho']!=0){
-            echo "\t\t<td>Hecho</td>\n";
-          }else{
-            echo "\t\t<td>Por Hacer</td>\n";
-          }         
-          echo "\t</tr>\n";
-      }*/
       echo "</table>\n";
-      // Liberando el conjunto de resultados
-      pg_free_result($result);
-
-      // Cerrando la conexión
-      pg_close($dbconn);
    ?>
   </body>
