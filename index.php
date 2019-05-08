01d61e8a2c453aaf4448f1576938117ea86c51c5
Nilarrus <nilarrus69@gmail.com>
2019-05-08 19:09:51 +0200

    gestior general 5

--- index.php
+++ index.php
@@ -19,23 +19,17 @@
 
  //echo "GET: ".var_dump($_GET);
  if(isset( $_GET['delete'] )){
-   //echo "En el get";
-   $dbconn = pg_connect("host=ec2-107-21-224-76.compute-1.amazonaws.com dbname=d9tf9mvi6tvf71 user=xrnnfbpijdpmin password=e2f25edc7569735ac66c311c993f760c258fbdbb19a97e7650d1d6524cf9da80")
-   or die('No se ha podido conectar: '.pg_last_error());
-   $id = $_GET['delete'];
-   
-   $result = pg_prepare($dbconn,"my_query",'DELETE FROM mytasks WHERE id LIKE $1;');
-   
-   $result = pg_execute($dbconn,"my_query",$id);
+    $task = $_GET['delete'];
+    $query = $db->prepare("DELETE FROM mytasks WHERE id = ".$task.";");
+    $query->execute();
  }
   if($_SERVER["REQUEST_METHOD"] == "POST"){
     $nom = $_POST['nom'];
     $query = $db->prepare("INSERT INTO mytasks (descripcio,hecho) VALUES ('".$nom."',0)");
     $query->execute();
   }
-   
+   //mostrar todo
     $result = $db->query("SELECT * FROM mytasks");
-    //echo "Resultat: ".var_dump($result);
   ?>
   <body>
     <h1>TASKAS EDITYON</h1>
@@ -56,9 +50,9 @@
           echo "\t\t<td>--".$row['id']."--</td>\n";
           echo "\t\t<td>".$row['descripcio']." <a href='index.php?delete=".$row['id']."'>Eliminar</a></td>\n";
           if($row['hecho']!=0){
-            echo "\t\t<td>Hecho</td>\n";
+            echo "\t\t<td>Hecho <a href='index.php?PorHacer=".$row['id']."'>Cambiar</a></td>\n";
           }else{
-            echo "\t\t<td>Por Hacer</td>\n";
+            echo "\t\t<td>Por Hacer <a href='index.php?hecho=".$row['id']."'>Cambiar</a></td>\n";
           }         
           echo "\t</tr>\n";
       }
